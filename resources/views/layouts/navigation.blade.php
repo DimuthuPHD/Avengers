ff018c44ed4e7c5d9e046ed901ea438ed1eb6257
Dimuthu <dimuthu@saberion.lk>
2023-11-07 23:15:04 +0530

    Route partial commi

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('route.create')" :active="request()->routeIs('route.create')">
+                        {{ __('route') }}
+                    </x-nav-link>
                 </div>
             </div>
 
